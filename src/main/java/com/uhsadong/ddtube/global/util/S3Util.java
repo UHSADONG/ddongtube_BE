b3720126fe0d3f60c5193c537d7778743ccb2c40
Ga Dong Sik <62535229+eple0329@users.noreply.github.com>
2025-03-28 01:21:44 +0900

    [USD-25] fix: S3 업로드 확장자 에러 해결 및 ACL 설정
    * [USD-25] feat: S3 ACL 퍼블릭으로 설정
    
    * [USD-25] fix: 파일 업로드 에러 해결
    
    * [USD-25] fix: S3 업로드 확장자 에러

--- src/main/java/com/uhsadong/ddtube/global/util/S3Util.java
+++ src/main/java/com/uhsadong/ddtube/global/util/S3Util.java
@@ -1,18 +1,22 @@
 package com.uhsadong.ddtube.global.util;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.uhsadong.ddtube.global.response.code.status.ErrorStatus;
 import com.uhsadong.ddtube.global.response.exception.GeneralException;
 import java.io.IOException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class S3Util {
 
     private final AmazonS3 amazonS3;
@@ -30,7 +34,15 @@ public class S3Util {
         metadata.setContentType(file.getContentType());
 
         try {
-            amazonS3.putObject(bucketName, fileName, file.getInputStream(), metadata);
+            PutObjectRequest putObjectRequest = new PutObjectRequest(
+                bucketName,
+                fileName,
+                file.getInputStream(),
+                metadata
+            ).withCannedAcl(CannedAccessControlList.PublicRead);
+
+            amazonS3.putObject(putObjectRequest);
+
         } catch (IOException e) {
             throw new GeneralException(ErrorStatus._FILE_UPLOAD_ERROR);
         }
@@ -41,10 +53,10 @@ public class S3Util {
     private String buildFileName(String originalFilename) {
         String uuid = UUID.randomUUID().toString();
         String ext = originalFilename.substring(originalFilename.lastIndexOf('.'));
-        return String.format("/%s%s", uuid, ext); // 예: images/uuid.jpg
+        return String.format("%s%s", uuid, ext); // 예: images/uuid.jpg
     }
 
-    public boolean isS3Url(String url){
+    public boolean isS3Url(String url) {
         String s3Prefix1 = String.format("https://%s.s3.%s.amazonaws.com/", bucketName, region);
 
         return url.startsWith(s3Prefix1);
