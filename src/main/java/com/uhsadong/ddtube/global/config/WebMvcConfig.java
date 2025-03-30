81ec63d2d2356cd07ae988ea93bd20c498e2a930
Ga Dong Sik <ret0422@gmail.com>
2025-03-30 17:58:23 +0900

    [USD-33] feat: CORS 세팅

--- src/main/java/com/uhsadong/ddtube/global/config/WebMvcConfig.java
+++ src/main/java/com/uhsadong/ddtube/global/config/WebMvcConfig.java
@@ -7,6 +7,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -19,5 +20,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new CurrentUserArgumentResolver(jwtUtil, userRepository));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*") // 허용할 출처 : 특정 도메인만 받을 수 있음
+            .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP method
+            .allowCredentials(true); // 쿠키 인증 요청 허용
     }
 }
