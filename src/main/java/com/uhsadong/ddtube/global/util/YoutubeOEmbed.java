d662156aff6141b4f5319df333832fce0917763e
Ga Dong Sik <ret0422@gmail.com>
2025-04-01 16:12:50 +0900

    [USD-67] feat: 유튜브 링크가 이상할 때 에러 던짐

--- src/main/java/com/uhsadong/ddtube/global/util/YoutubeOEmbed.java
+++ src/main/java/com/uhsadong/ddtube/global/util/YoutubeOEmbed.java
@@ -1,7 +1,11 @@
 package com.uhsadong.ddtube.global.util;
 
 import com.uhsadong.ddtube.domain.dto.YoutubeOEmbedDTO;
+import com.uhsadong.ddtube.global.response.code.status.ErrorStatus;
+import com.uhsadong.ddtube.global.response.exception.GeneralException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -20,6 +24,13 @@ public class YoutubeOEmbed {
 
         String uri = builder.build(false).toUriString();
 
-        return restTemplate.getForObject(uri, YoutubeOEmbedDTO.class);
+        try {
+            return restTemplate.getForObject(uri, YoutubeOEmbedDTO.class);
+        } catch (HttpClientErrorException e) {
+            if (e.getStatusCode() == HttpStatus.BAD_REQUEST) {
+                throw new GeneralException(ErrorStatus._YOUTUBE_OEMBED_BAD_REQUEST);
+            }
+            throw e; // 다른 에러는 그대로 던짐
+        }
     }
 }
